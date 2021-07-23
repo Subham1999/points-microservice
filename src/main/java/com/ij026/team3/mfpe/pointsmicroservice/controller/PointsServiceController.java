086be6f1ca15b9af6ad511caf7c037a6ce4c3fd3
gurusai <51899461+gurusai123@users.noreply.github.com>
2021-07-23 12:02:41 +0530

    GlobalException

--- src/main/java/com/ij026/team3/mfpe/pointsmicroservice/controller/PointsServiceController.java
+++ src/main/java/com/ij026/team3/mfpe/pointsmicroservice/controller/PointsServiceController.java
@@ -1,6 +1,7 @@
 package com.ij026.team3.mfpe.pointsmicroservice.controller;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +10,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ij026.team3.mfpe.pointsmicroservice.exception.NoSuchEmpIdException;
 import com.ij026.team3.mfpe.pointsmicroservice.service.PointsService;
 
 @RestController
 public class PointsServiceController {
 	@Autowired
 	private PointsService pointsService;
+	private ConcurrentHashMap<String, Object> empIdCache = new ConcurrentHashMap<>();
+
+	public PointsServiceController() {
+		empIdCache.put("guru", new Object());
+		empIdCache.put("nikky", new Object());
+		empIdCache.put("subsa", new Object());
+		empIdCache.put("rish", new Object());
+		empIdCache.put("ujjw", new Object());
+	}
 
 	@GetMapping("/test")
 	public String test(@RequestParam(required = false) Map<String, Object> map) {
@@ -24,7 +35,12 @@ public class PointsServiceController {
 
 	@GetMapping("/getPointsOfEmployee/{empId}")
 	public ResponseEntity<Integer> getPointsOfEmployee(@PathVariable String empId) {
+		if(empIdCache.contains(empId)) {
 		return ResponseEntity.ok(pointsService.calculatePointsOfEmployee(empId));
+		}
+		else {
+			throw new NoSuchEmpIdException("empid " + empId + " is invalid");
+		}
 	}
 
 }
